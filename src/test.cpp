cc3f066f1ddcbef8608e6390f0720cef7a9aa6bf
Carl Malmz <callem217@gmail.com>
2016-09-28 16:59:39 +0200

    fixed tabbing

--- src/test.cpp
+++ src/test.cpp
@@ -2,12 +2,12 @@
 using namespace std;
 
 int main() {
-		int x[5];
-		for(int i=0;i < 4; i++) {
-				cin >> x[i];
-		}
-		for(int i=0;i < 4; i++) {
-				cout << x[i];
-		}
-		return 0;
+	int x[5];
+	for(int i=0;i < 4; i++) {
+		cin >> x[i];
+	}
+	for(int i=0;i < 4; i++) {
+		cout << x[i];
+	}
+	return 0;
 }
